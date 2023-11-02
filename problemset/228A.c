50f2ec8df435eff977005979a7fbe150c8f7ff06
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2023-11-02 18:46:00 -0300

    fix: new code (incomplete)

--- problemset/228A.c
+++ problemset/228A.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
-
+//não funcionando para todos os casos
 int main()
 {
     long int colors[4];
     long int aux[4];
-    int i, j;
     int count = 0;
-    int same_color;
 
-    for (i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; ++i) {
         scanf("%ld", &colors[i]);
     }
 
-    aux[0] = colors[0];
+    colors[0] = aux[0];
 
-    for (i = 1; i < 4; i++) {
+    for (int i = 1; i < 4; i++) {
+
+        for (int j = 0; j < i; j++) {
+            if (colors[i] == aux[j]) {
+
+                count++;
+            }
+        }
 
         aux[i] = colors[i];
-        for (j = 0; j < 4; i++) {
-            if (colors) //if ... in ... (python)
-        }
     }
+
+    printf("%d", count);
 
     return 0;
 }
