c4e03225e131525978f41cec65ea7144c406df97
Breno de Carvalho <88058952+brenovsky@users.noreply.github.com>
2023-11-09 20:38:32 -0300

    add: finalcode 228A in C

--- problemset/228A.c
+++ problemset/228A.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-//não funcionando para todos os casos!!
+
 int main()
 {
-    long int colors[4];
-    long int aux[4];
-    int count = 0;
+	int colors[4];
+	int colors2[4] = {0, 0, 0, 0};
+	int count = 0;
+	
+	for (int i = 0; i < 4; i += 1) {
+		scanf("%d", &colors[i]);
+	}
 
-    for (int i = 0; i < 4; ++i) {
-        scanf("%ld", &colors[i]);
-    }
-
-    colors[0] = aux[0];
-
-    for (int i = 1; i < 4; i++) {
-
-        for (int j = 0; j < i; j++) {
-            if (colors[i] == aux[j]) {
-
-                count++;
-            }
-        }
-
-        aux[i] = colors[i];
-    }
-
-    printf("%d", count);
-
-    return 0;
+	for (int i = 0; i < 4; i += 1) {
+		for (int j = 0; j < 4; j += 1) {
+			if (colors2[j] == colors[i]) {
+				count += 1;
+				break;
+			}
+		}
+		colors2[i] = colors[i];
+	}
+	
+	printf("%d", count);
+	
+	return 0;
 }
