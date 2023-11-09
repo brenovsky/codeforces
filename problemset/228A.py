ddd68ed8266d1c08de3a9254339d4addc3c445f6
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2023-11-09 13:19:50 -0300

    add: 228A (python)

---
+++ problemset/228A.py
@@ -0,0 +1,12 @@
+colors = list(map(int, input().split()))
+
+colors2 = [] #without repetitions
+
+counter = 0
+
+for i in range(4):
+    if colors[i] in colors2:
+        counter += 1
+    colors2.append(colors[i])
+
+print(f'{counter}')
